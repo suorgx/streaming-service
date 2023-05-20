c8d92a0d3875060293f9730ac45f7957405ca220
suorgx <xsuorgx@gmail.com>
2023-05-20 07:10:29 +0300

    update auth page; add state login & register

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import Image from 'next/image';
 import Input from "@/components/input";
 
@@ -10,10 +10,16 @@ export default function Auth() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const [variant, setVariant] = useState('login')
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+  }, [])
+
   return (
-    <div className='relative h-full w-full bg-[url("/images/hero.png")] bg-cover bg-bottom'>
+    <div className='relative h-full w-full sm:bg-[url("/images/hero.png")] bg-black bg-cover bg-left'>
       <div className='h-full w-full bg-black bg-opacity-50'>
-        <nav className='px-12 py-5'>
+        <nav className='sm:px-8 sm:py-5 lg:px-12 p-4'>
           <Image
             className='h-auto w-auto'
             src='/images/logo.png'
@@ -22,19 +28,21 @@ export default function Auth() {
             alt='logo'
           />
         </nav>
-        <div className='flex justify-center h-3/4'>
-          <div className='bg-black bg-opacity-70 md:p-16 sm:p-8 p-4 lg:w-2/5 md:w-3/5 sm:w-4/5 w-full rounded-md self-center'>
+        <div className='flex justify-center'>
+          <div className='mt-20 bg-black bg-opacity-70 md:p-16 sm:p-8 p-4 lg:w-2/5 md:w-3/5 sm:w-4/5 w-full rounded-md self-center'>
             <h2 className='text-3xl text-white mb-8 font-medium'>
-              Sign in
+              {variant === 'login' ? 'Sign in' : 'Register'}
             </h2>
             <div className='flex flex-col gap-4'>
-              <Input
-                value={name}
-                id='login'
-                label='Username'
-                onChange={(e: any) => setName(e.target.value)}
-                type='text'
-              />
+              {variant === 'register' && (
+                <Input
+                  value={name}
+                  id='login'
+                  label='Username'
+                  onChange={(e: any) => setName(e.target.value)}
+                  type='text'
+                />
+              )}
               <Input
                 value={email}
                 id='email'
@@ -50,11 +58,13 @@ export default function Auth() {
                 type='password'
               />
               <button className='bg-red-500 mt-4 w-full rounded-md py-3 text-white hover:bg-red-600 transition'>
-                Login
+                {variant === 'login' ? 'Login' : 'Sign Up'}
               </button>
               <div className='text-neutral-500 mt-6'>
-                Don‘t have an account?
-                <span className='text-white ml-1 hover:underline cursor-pointer'>Sign Up</span>
+                {variant === 'login' ? 'Don‘t have an account?' : 'Already have an account?'}
+                <span onClick={toggleVariant} className='text-white ml-1 hover:underline cursor-pointer'>
+                  {variant === 'login' ? 'Create an account' : 'Login'}
+                </span>
               </div>
             </div>
           </div>
